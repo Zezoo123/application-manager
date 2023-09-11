c33752a96af02f4d6d4867ccb651131265f7f8a8
Zezoo123 <zyadsalama14@gmail.com>
2023-09-11 12:00:03 +0300

    router created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express()
 
 const db = require("./models");
 
+// Routers 
+const loginRouter = require("./routes/login");
+app.use("/login", loginRouter);
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log('Server running on port 3001');
